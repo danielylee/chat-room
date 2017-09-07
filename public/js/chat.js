d0d05146999186b826fc5e1d8051a0c4b394f9aa
danielylee <daniel.y.lee9@gmail.com>
2017-09-06 21:08:21 -0700

    added user functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -37,6 +37,16 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
+socket.on('updateUserList', function (users) {
+  var ol = $('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append($('<li></li>').text(user));
+  });
+
+  $('#users').html(ol);
+});
+
 socket.on('newMessage', function (msg) {
   var tmpl = $('#message-template').html();
   var html = Mustache.render(tmpl, {
